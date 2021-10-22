16d467880e9d408a6814fd854ac4edd26efce718
Lucas <80311627+Lucgome10@users.noreply.github.com>
2021-10-22 18:05:43 +0200

    Corregidas tabulaciones

--- UNIDAD 1/Actividad3.java
+++ UNIDAD 1/Actividad3.java
@@ -5,34 +5,34 @@ import java.io.File;
 public class Actividad3 {
 
 	/*
-	    Haciendo uso del código de la actividad anterior, modifica el programa para que además muestre
-		información al usuario sobre el número total de ficheros y directorios que se encuentran dentro del
+	    Haciendo uso del cÃ³digo de la actividad anterior, modifica el programa para que ademÃ¡s muestre
+		informaciÃ³n al usuario sobre el nÃºmero total de ficheros y directorios que se encuentran dentro del
 		directorio actual.
 	*/
 	public static void main(String[] args) {
 		
-		// Creación de array con los archivos del directorio.
+		// CreaciÃ³n de array con los archivos del directorio.
 		File f = new File(".");	
-        File[] archivos = f.listFiles();
+        	File[] archivos = f.listFiles();
 
         
-        // Inicializando contadores a los que asignaré el total de archivos y directorios. 
-        int numArchivos=0;
+        // Inicializando contadores a los que asignarÃ© el total de archivos y directorios. 
+        	int numArchivos=0;
 		int numDirectorios=0;
         
 		
 		// Trabajo con archivos:
-        System.out.println("Listado de archivos:");
+        	System.out.println("Listado de archivos:");
 		for (int i = 0; i < archivos.length; i++) {
 			if (archivos[i].isFile()) {
-				System.out.println(" " + archivos[i].getName()); //Impresión del nombre de los archivos.
+				System.out.println(" " + archivos[i].getName()); //ImpresiÃ³n del nombre de los archivos.
 				numArchivos++;
 			}
 		}
-		System.out.println("En total hay " + numArchivos + " archivos en la carpeta.\n"); //Impresión del total de archivos.
+		System.out.println("En total hay " + numArchivos + " archivos en la carpeta.\n"); //ImpresiÃ³n del total de archivos.
 		
 		
-		// Trabajo con directorios: contempla que los directorios puedan a su vez tener máximo un directorio más en su interior.
+		// Trabajo con directorios: contempla que los directorios puedan a su vez tener mÃ¡ximo un directorio mÃ¡s en su interior.
 		System.out.println("Listado de directorios:");
 		for (int i = 0; i < archivos.length; i++) {
 			if (archivos[i].isDirectory()) {
@@ -43,10 +43,10 @@ public class Actividad3 {
 				numDirectorios++;
 			}
 		}
-		System.out.println("En total hay " + numDirectorios + " directorios en la carpeta.\n"); //Impresión del total de directorios.
+		System.out.println("En total hay " + numDirectorios + " directorios en la carpeta.\n"); //ImpresiÃ³n del total de directorios.
 		
-        System.out.println("\ngetParent:" + f.getParent());
-        System.out.println("getPath:" + f.getPath());
+        	System.out.println("\ngetParent:" + f.getParent());
+        	System.out.println("getPath:" + f.getPath());
 
 	}
 
