6e7edd9dfb2fd3a2d7081bda7d932b3fad0b328b
Lucas <80311627+Lucgome10@users.noreply.github.com>
2021-11-19 21:35:33 +0100

    Update Actividad14.java

--- UNIDAD 1/Actividad14/Actividad14.java
+++ UNIDAD 1/Actividad14/Actividad14.java
@@ -9,8 +9,6 @@ public class Actividad14 {
 		File file1 = new File("src\\actividad14\\file1.txt");
 		File file2 = new File("src\\actividad14\\file2.txt");
 		
-		
-		File file = new File("/mnt/sdcard/abc.txt");
 		LineNumberReader lineNumberReader1;
 		LineNumberReader lineNumberReader2;
 		int lines1 = 0;
@@ -29,8 +27,8 @@ public class Actividad14 {
 			e1.printStackTrace();
 		}
 		
-		System.out.println("file1 tiene " + lines1 + " líneas");
-		System.out.println("file2 tiene " + lines2 + " líneas");
+		System.out.println("file1 tiene " + lines1 + " lÃ­neas");
+		System.out.println("file2 tiene " + lines2 + " lÃ­neas");
 
 		
 		BufferedReader br1;
@@ -50,7 +48,7 @@ public class Actividad14 {
 				line2 = br2.readLine();
 				
 				if (!line1.equals(line2)) {
-					System.out.println("Diferencia encontrada en la línea " + numLinea + ": " + "\n\tFile1: " + line1 + "\n\tFile2: " + line2);
+					System.out.println("Diferencia encontrada en la lÃ­nea " + numLinea + ": " + "\n\tFile1: " + line1 + "\n\tFile2: " + line2);
 				}
 			} while (line1!=null && line2!=null);
 		} catch (FileNotFoundException e) {
